870dac3a3c53290c6947df07d0675150fc8bfcf2
Бугаев Роман <roman15082002@mail.ru>
2025-10-03 14:31:38 +0300

    fix(lab2.3): реализует динамическое добавление иконок в список навыков

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 'use strict';
 
-function SkillData(name , level, cssClass){
+function SkillData(name , level, cssClass, icon){
     this.name = name;
     this.level = level;
     this.cssClass = cssClass;
+    this.iconName = icon;
 }
 
 const data = [
-    new SkillData('Си', 80, 'skill-item_c' ),
-    new SkillData('С++', 50, 'skill-item_cpp' ),
-    new SkillData('FPGA', 60, 'skill-item_fpga' ),
-    new SkillData('Python', 50, 'skill-item_python' ),
+    new SkillData('Си', 80, 'skill-item_c', 'c.svg'),
+    new SkillData('С++', 50, 'skill-item_cpp', 'c++.svg'),
+    new SkillData('FPGA', 60, 'skill-item_fpga', 'fpga.svg'),
+    new SkillData('Python', 50, 'skill-item_python', 'python.svg'),
 ];
 
 
 const skillList = document.querySelector('.skill-list')
 
 data.forEach(item => {
-    console.log(item);
 
     const dd = document.createElement('dd');
     const dt = document.createElement('dt');
@@ -28,6 +28,7 @@ data.forEach(item => {
     dd.classList.add('skill-level');
 
     dt.innerText = item.name;
+    dt.style.backgroundImage = `url('./img/skills/${item.iconName}')`;
 
     div.style.width = item.level + '%';
 
