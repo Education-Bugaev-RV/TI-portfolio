a2bc07e09103a747ab13c8f7dfb71df2365da5e0
Бугаев Роман <roman15082002@mail.ru>
2025-10-03 14:46:58 +0300

    fix(lab2.3): реализует код в объектном стиле

--- js/script.js
+++ js/script.js
@@ -7,33 +7,38 @@ function SkillData(name , level, cssClass, icon){
     this.iconName = icon;
 }
 
-const data = [
-    new SkillData('Си', 80, 'skill-item_c', 'c.svg'),
-    new SkillData('С++', 50, 'skill-item_cpp', 'c++.svg'),
-    new SkillData('FPGA', 60, 'skill-item_fpga', 'fpga.svg'),
-    new SkillData('Python', 50, 'skill-item_python', 'python.svg'),
-];
+const skills = {
+    'data' : [
+        new SkillData('Си', 80, 'skill-item_c', 'c.svg'),
+        new SkillData('С++', 50, 'skill-item_cpp', 'c++.svg'),
+        new SkillData('FPGA', 60, 'skill-item_fpga', 'fpga.svg'),
+        new SkillData('Python', 50, 'skill-item_python', 'python.svg'),
+    ],
+    'generateList': function(skillList) {
+        this.data.forEach(item => {
 
+            const dd = document.createElement('dd');
+            const dt = document.createElement('dt');
+            const div = document.createElement('div');
+
+            dt.classList.add('skill-item');
+            dt.classList.add(item.cssClass);
+            dd.classList.add('skill-level');
+
+            dt.innerText = item.name;
+            dt.style.backgroundImage = `url('./img/skills/${item.iconName}')`;
+
+            div.style.width = item.level + '%';
+
+            dd.append(div);
+
+            skillList.append(dt);
+            skillList.append(dd);
+
+        });
+    }
+}
 
 const skillList = document.querySelector('.skill-list')
 
-data.forEach(item => {
-
-    const dd = document.createElement('dd');
-    const dt = document.createElement('dt');
-    const div = document.createElement('div');
-
-    dt.classList.add('skill-item');
-    dt.classList.add(item.cssClass);
-    dd.classList.add('skill-level');
-
-    dt.innerText = item.name;
-    dt.style.backgroundImage = `url('./img/skills/${item.iconName}')`;
-
-    div.style.width = item.level + '%';
-
-    dd.append(div);
-
-    skillList.append(dt);
-    skillList.append(dd);
-});
+skills.generateList(skillList);
